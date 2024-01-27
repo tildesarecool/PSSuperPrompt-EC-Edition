264d4fa778cb84ba597dd3c746580db7a249abe3
tildesarecool <tildesare.cool@gmail.com>
2024-01-26 17:47:40 -0800

    Did further organization and separating sections into functions. Still a few hours work left.

--- README.md
+++ README.md
@@ -1,14 +1,45 @@
-# PSSuperPrompt
-
-Some cool extra tricks for your PowerShell prompt to display more details and colors
+# PSSuperPrompt "EC Edition"
 
 This is a fork of [PSSuperPrompt](https://github.com/poshcodebear/PSSuperPrompt).
 
-Bearing ind mind of course I don't actually know what the purpose of it is or if I'd find it useful. 
+Basic description
+>Some cool extra tricks for your PowerShell prompt to display more details and colors
+
+Bearing in mind of course I don't actually know what the purpose of it is or if I'd find it useful. 
+
+###Requirements (Windows):
+- **PS v7.x** for Windows 10/11
+
+- *Local admin/root access to system* in order to fully customize prompt (and run programs with local admin)
+
+###Optional but good to have:
+* **Git for Windows** (available via package manager like chocolatey or [the git website](https://gitforwindows.org/)
+  - choco command: 
+  **choco install git**
+
+- **[PS module posh-git](https://dahlbyk.github.io/posh-git/)** (recommended by original author)
+
+  - Upon searching I found poshgit listed as a package in the chocolatey repo, but I haven't tried it yet. The command as above would be
+  **choco install poshgit**
+
+- **Chocolatey package manager** for windows: my own preference for Windows package managers (I imagine __win-get__ will server this purpose too)
+  
+  - Install instructions with a copy/paste installer script can be [found on the chocolatey install guide page](https://chocolatey.org/install).
+  - As it turns out there's several ways of installing posh-git that involves a few more steps depending on a number of variables. I'm linking to the instructions on the project's GitHub and leave it at that: 
+  https://github.com/dahlbyk/posh-git
+
+###Installation
+
+Scientists are still working on deciphering the meaning of this non-sensical technobabble. That's what I'm going with.
+
+> To install it, just paste the entire thing into your `$profile`, or dot source the .ps1 file from your `$profile`. Either will work.
+
+
+## Latest Updates will be at the bottom
 
 I've already worked on it a little bit before the forking. Although re-reading these notes after forking I realize I didn't follow the instructions in my testing and attempted use. *I may just apply to be a software tester.*
 
-I'll go thorugh the notes here and make some *useful* comments of my own.
+I'll go through the notes here and make some *useful* comments of my own.
 
 The formatting seems to be a little off versus the original but I haven't got around to fixing it. I blame CR/LF.
 
@@ -24,7 +55,7 @@ obviously not on my radar. I would love to add that, though, so if I get around 
 someone likes this starting point and wants to shoot me a pull request with extra bells and
 whistles, it may very well get those features in the future.
 
- I thinking of planning on thinking of adding some customization options. With a...menu? Or a make the user edit a JSON. Maybe both. 
+ I'm thinking of planning on thinking of adding some customization options. With a...menu? Or  make the user edit a JSON. Maybe both. 
 
 >- The git functions require the module posh-git to be installed. It tests for this and will work
 perfectly fine without it, you just won't get any git details in git repositories. It does use
@@ -38,17 +69,19 @@ not getting the added delay every time.
 prompt easier so I don't have to reload every time I try something, or if I set up another prompt
 it lets me switch back to this one with four keystrokes.
 
-Actually I stared at these lines while working on this is it didn't register what these were for. I was about to suggest "a way to reset back to default". Then...well never mind then.
+Actually I stared at these lines while working on this is it didn't register what these were for. I was about to suggest "a way to reset back to default". Then...well never mind then. Update: I couldn't get the alias to work so I don't know how to revert back to the old prompt.
 
 >- To install it, just paste the entire thing into your `$profile`, or dot source the .ps1 file
 from your `$profile`. Either will work.
 
 I think I'll move this minor detail to the top of the description and provide more details on what that means and how to do it. As soon as I fugre that out...I'm kidding I'm kidding. I probably know what that means.
+Update: as it turns out, typing `$profile` into powershell simply provides the output of a path to the file
+**Microsoft.PowerShell_profile.ps1**
+So I think this line means...paste the script into that PS1 file? Did I get it right? Or put prompt.ps1 into the same path as Microsoft.PowerShell_profile.ps1? Probably one of those two things.
 
 >- If I wind up taking the time, at some point I plan to add a better description and do a lot of the
 cleanup and configurability stuff I mentioned above.
-
-> - That said, I know myself too well; please don't hold your breath, as it could happen any time between now and the heat death of the universe, and probably closer to the latter than the former ;)
+>   - That said, I know myself too well; please don't hold your breath, as it could happen any time between now and the heat death of the universe, and probably closer to the latter than the former ;)
 
 ![](thanos-fine-i'll-do-it-myself-ascii-20percent-with-caption.jpg "This only took me a few minutes. Too long.")
 
@@ -73,3 +106,23 @@ Now *this* is fun...or weird. Definitely one of those two.
 Also, Gentoo? You just use that so you can take a break while you compile your kernel for the 5th time today, aren't yoU?
 
 I use Arch btw.
+
+---
+
+###Starting Work
+
+This scripts starts with some extended tests to see if the OS is windows or Linux for purpose of determining if it's being run under an Admin/root level shell. 
+
+After very little work it seems likely some how this OS answer will have to branch out for the two possibilities from the start. In other words ask the OS question and pass that in the main function then take appropriate actions depending on the OS.
+
+Having said that I'm going to re-write it until it works on Windows then go back when I have a Linux environment and add that in. Or via Ubunut-bash-Windows, because of how not convoluted that sounds (running PS 7 via bash via ubuntu via Windows...nailed it. Oh even better: PS 7 via bash via ubuntu via Windows...in a docker container).
+
+I was having trouble with detecting the posh-git module so I did a little experimenting and this actually runs on the PS command line:
+
+```Powershell
+if (get-module -Name posh* -ListAvailable -Verbose:$false) { Write-Host "found" } else { Write-Host "not found" }
+
+```
+As I'm still troubleshooting without posh-git, this returns *not found* for me. But I could also set a variable to either True or False depending instead. I mean it's a few lines longer to create a function to do this but I know it works.
+
+
--- prompt.ps1
+++ prompt.ps1
@@ -1,127 +1,24 @@
-$Global:__poshGitInstalled = [bool](Get-Module -Name posh-git -ListAvailable -Verbose:$false)
+# have to speculate on the purpose of these two lines
+# I think it's a double test against poshgit module being installed:
+# the first test with the get-module of posh-git is test
+#$Global:__poshGitInstalled = [bool](Get-Module -Name posh-git -ListAvailable -Verbose:$false)
+# and the second test combines with "Get-GitStatus" for this $gitstatus variable
+#$gitStatus = $Global:__poshGitInstalled -and (Get-GitStatus)
+# later in the script, it tests against $gitStatus to perform further tests
+# But since I don't have sample output of what Get-GitStatus returns I'm kind of guessing
+# so it could be it's saying "make sure the posh-git module is installed then also get result of a Get-GitStatus command"
+# assuming that Get-GitStatus would return false or non-true if it failed. which seems unnecessary
+# so I'm taking over the gitstatus variable to simply check if git-for-windows is installed or not instead
+# a function posh-git already wrote out so I'm going to "borrow" it
 
-function Global:prompt
-{
-    $wd = (Get-Location).Path
-    $user = (whoami).Split('\')[-1]
-    $hostname = hostname
-    $ver = ([string]$PSVersionTable.PSVersion).Split('.')[0..1] -join '.'
-    $jobs = Get-Job
-    
-    if ($ver -ge 7)
-        {$lastTime = Get-LastHistoryDuration}
-    # Extra versiony goodness:
-    if ($null -ne $PSVersionTable.PSVersion.Patch)
-    {
-        $ver += ".$($PSVersionTable.PSVersion.Patch)"
+function DetermineGitInstalled {
+    GitMissing = $true
+    if ( (Get-Command git  -ErrorAction SilentlyContinue)  ) { 
+        GitMissing = $false
+    } else {
+        GitMissing = $true
     }
-    
-    $admin = $false
-    if ($IsLinux -and $user -eq 'root')
-    {
-        $admin = $true
-    }
-    elseif (!$IsLinux)
-    {
-        $admin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
-    }
-    
-    $gitStatus = $Global:__poshGitInstalled -and (Get-GitStatus)
-    
-    if ($wd -eq $HOME)
-    {
-        $wd = '~'
-    }
-    elseif ($wd.Split('\')[-1] -ne '' -and !$gitStatus)
-    {
-        $wd = $wd.Split('\')[-1]
-    }
-    elseif ($gitStatus -and $wd -like '*::*')
-    {
-        $wd = $wd.Split(':')[-1]
-    }
-    
-    # History count:
-    Write-Host "$(((Get-History).Count + 1).ToString().PadLeft(3, '0')) " -NoNewline
-    # PS Version:
-    if ($PSVersionTable.PSVersion.PreReleaseLabel)
-    {
-        Write-Host "($($ver)-$($PSVersionTable.PSVersion.PreReleaseLabel)) " -ForegroundColor Red -NoNewline
-    }
-    else
-    {
-        Write-Host "($ver) " -ForegroundColor Magenta -NoNewline
-    }
-    # Timestamp:
-    Write-Host "[$(([string](Get-Date)).Split()[1])] " -ForegroundColor Yellow -NoNewline
-    
-    # Last Execution Time:
-    if ($ver -ge 7 -and $lastTime)
-    {
-        Write-Host "<$($lastTime)> " -ForegroundColor Blue -NoNewline
-    }
-    
-    # Job control:
-    if ($jobs)
-    {
-        $r = $jobs.where({$_.State -eq 'Running'}).count
-        $d = $jobs.where({$_.State -eq 'Completed' -and $_.HasMoreData}).count
-        $c = $jobs.where({$_.State -eq 'Completed' -and -not $_.HasMoreData}).count
-        
-        $jobCounter = "jobs($($c)/$($jobs.Count))"
-        
-        Write-Host '[' -NoNewline -ForegroundColor Yellow
-        if ($d) {Write-Host $jobCounter -NoNewline -ForegroundColor Red}
-        elseif ($c -eq $jobs.Count)
-                {Write-Host $jobCounter -NoNewline -ForegroundColor Green}
-        else    {Write-Host $jobCounter -NoNewline -ForegroundColor Cyan}
-        Write-Host ' : ' -NoNewline -ForegroundColor Gray
-        # Running
-        Write-Host ">$r " -NoNewline -ForegroundColor Cyan
-        # HasData
-        Write-Host "!$d " -NoNewline -ForegroundColor Red
-        # Complete
-        Write-Host "`$$c" -NoNewline -ForegroundColor Green
-        
-        Write-Host '] ' -NoNewline -ForegroundColor Yellow
-    }
-    
-    if ($gitStatus)
-    {
-        Write-Host '[GIT] ' -ForegroundColor Green -NoNewline
-    }
-    else
-    {
-        
-        if ($admin)
-        {
-            Write-Host "$($user)" -ForegroundColor Red -NoNewLine
-        }
-        else
-        {
-            Write-Host "$($user)" -ForegroundColor Green -NoNewLine
-        }
-        Write-Host " @ " -ForegroundColor Yellow -NoNewLine
-        Write-Host "$($hostname) " -ForegroundColor Magenta -NoNewLine
-    }
-    if ($admin)
-    {
-        $sym = '#'
-    }
-    else
-    {
-        $sym = '$'
-    }
-    Write-Host "$($wd) " -ForegroundColor Cyan -NoNewLine
-    if ($gitStatus)
-    {
-        Write-Host "`b" -NoNewline
-        Write-Host (Write-VcsStatus) -NoNewline
-    }
-    #>
-    Write-Host ''
-    Write-Host "$($sym)" -ForegroundColor Cyan -NoNewLine
-    return ' '
+    return GitMissing
 }
 
 function Get-LastHistoryDuration
@@ -137,17 +34,204 @@ function Get-LastHistoryDuration
         $seconds = $duration.Seconds.ToString().PadLeft(2, '0')
         $milliseconds = $duration.Milliseconds.ToString().PadLeft(3, '0')
         $output = "$($seconds).$($milliseconds)"
-        if ($minutes -ne '00')
-        {
-            $output = "$($minutes):$($output)"
+        
+        if ($minutes -ne '00') {
+            $output = "$($minutes):$($output)"        
         }
-        if ($hours -ne '00')
-        {
+        if ($hours -ne '00') {
             $output = "$($hours):$($output)"
         }
         
         return $output
     }
+}
+
+function Test-Administrator {
+    # $admin = $false
+     
+#    if ($IsLinux -and $user -eq 'root')
+#    {
+#        $admin = $true
+#    }
+$EUID = [System.Security.Principal.WindowsIdentity]::GetCurrent().User.Value
+     if ($IsLinux -and $user -eq 'root' -and $EUID -eq '0') {
+
+            $admin = $true
+
+        } elseif ($IsWindows) {
+         $identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+         $principal = New-Object System.Security.Principal.WindowsPrincipal($identity)
+         
+         $adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+         Write-Host "[System.Security.Principal.WindowsBuiltInRole]::Administrator is " $adminRole
+         $admin = $principal.IsInRole($adminRole)
+         Write-Host "admin after isinroll is" $admin
+     } else {
+         Write-Host "I dunno what OS you have"
+         $admin = $false
+      #   Write-Host $admin
+     }
+ 
+     Write-Host $admin
+     return $admin
+ }
+
+
+
+function TestForPosh {
+    if (get-module -Name posh* -ListAvailable -Verbose:$false) { 
+        Write-Host "found posh git" 
+        return $true
+    } else { 
+        Write-Host "did not find posh git, exiting...." 
+        return $false
+        
+    }
+}
+
+
+function setWorkDir {
+    $workdir = (Get-Location).Path
+    if ($workdir -eq $HOME) {
+        $workdir = '~'
+    } elseif ($workdir.Split('\')[-1] -ne '' -and !$gitStatus)    {
+        $workdir = $workdir.Split('\')[-1]
+    } elseif ($gitStatus -and $workdir -like '*::*') {
+        $workdir = $workdir.Split(':')[-1]
+    }
+    return $workdir
+}
+
+$Global:__poshGitInstalled = TestForPosh
+$Global:jobs = Get-Job
+function Global:prompt {
+
+    $wd = setWorkDir # (Get-Location).Path
+    # rather than splicing up the 'whoami' command you "could" refer to the %USERNAME$ environment variable. At least on windows
+    # I assume there's an equivalent env var under Linux but I don't know if it would be accessed the same way via PS 
+    #$user = (whoami).Split('\')[-1]
+    $user = $env:USERNAME
+    # $hostname = hostname # redundant variable of redundancy is redundant
+    $ver = ([string]$PSVersionTable.PSVersion).Split('.')[0..1] -join '.'
+#    $jobs = Get-Job
+    
+    if ($ver -ge 7) {
+        $lastTime = Get-LastHistoryDuration
+    }
+    # Extra versiony goodness:
+    if ($null -ne $PSVersionTable.PSVersion.Patch) {
+        $ver += ".$($PSVersionTable.PSVersion.Patch)"
+    }
+    
+#    $admin = $false
+#    if ($IsLinux -and $user -eq 'root')
+#    {
+#        $admin = $true
+#    }
+#    elseif (!$IsLinux)
+#    {
+#        $admin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
+#    }
+    
+    # the Get-GitStatus command doesn't exist by default. It probably comes with the posh-git module
+    # so stating posh-git is optional may be misleading slightly.
+    # this line I commented out
+    # $gitStatus = $Global:__poshGitInstalled -and (Get-GitStatus)
+    # after looking at the only file that contains Get-GitStatus on the posh-git repo,
+    # i am just guessing it shows the status of corrently selected git repo as opposed to just returning whether or not
+    # git is installed or not
+    
+################ Since I don't know what these few lines below does i'm commenting them out ################ 
+
+#    elseif ($wd.Split('\')[-1] -ne '' -and !$gitStatus)
+#    {
+#        $wd = $wd.Split('\')[-1]
+#    }
+#    elseif ($gitStatus -and $wd -like '*::*')
+#    {
+#        $wd = $wd.Split(':')[-1]
+#    }
+
+################ Since I don't know what these few lines below does i'm commenting them out ################ 
+    
+    # History count:
+    Write-Host "$(((Get-History).Count + 1).ToString().PadLeft(3, '0')) " -NoNewline
+    # PS Version:
+    if ($PSVersionTable.PSVersion.PreReleaseLabel) {
+        Write-Host "($($ver)-$($PSVersionTable.PSVersion.PreReleaseLabel)) " -ForegroundColor Red -NoNewline
+    }
+    else {
+        Write-Host "($ver) " -ForegroundColor Magenta -NoNewline
+    }
+    # Timestamp:
+    Write-Host "[$(([string](Get-Date)).Split()[1])] " -ForegroundColor Yellow -NoNewline
+    
+    # Last Execution Time:
+    if ($ver -ge 7 -and $lastTime) {
+        Write-Host "<$($lastTime)> " -ForegroundColor Blue -NoNewline
+    }
+    
+    # Job control:
+    if ($jobs) {
+        $r = $jobs.where({$_.State -eq 'Running'}).count
+        $d = $jobs.where({$_.State -eq 'Completed' -and $_.HasMoreData}).count
+        $c = $jobs.where({$_.State -eq 'Completed' -and -not $_.HasMoreData}).count
+        
+        $jobCounter = "jobs($($c)/$($jobs.Count))"
+        
+        Write-Host '[' -NoNewline -ForegroundColor Yellow
+        if ($d) {
+            Write-Host $jobCounter -NoNewline -ForegroundColor Red
+        }
+        elseif ($c -eq $jobs.Count) {
+            Write-Host $jobCounter -NoNewline -ForegroundColor Green
+        }
+        else {
+            Write-Host $jobCounter -NoNewline -ForegroundColor Cyan
+        }
+        Write-Host ' : ' -NoNewline -ForegroundColor Gray
+        # Running
+        Write-Host ">$r " -NoNewline -ForegroundColor Cyan
+        # HasData
+        Write-Host "!$d " -NoNewline -ForegroundColor Red
+        # Complete
+        Write-Host "`$$c" -NoNewline -ForegroundColor Green
+        
+        Write-Host '] ' -NoNewline -ForegroundColor Yellow
+    }
+    
+    if ($gitStatus) {
+        Write-Host '[GIT] ' -ForegroundColor Green -NoNewline
+    }
+    else  {
+        
+        if ($admin)  {
+            Write-Host "$($user)" -ForegroundColor Red -NoNewLine
+        }
+        else  {
+            Write-Host "$($user)" -ForegroundColor Green -NoNewLine
+        }
+        Write-Host " @ " -ForegroundColor Yellow -NoNewLine
+        #Write-Host "$($hostname) " -ForegroundColor Magenta -NoNewLine
+        # bro, 
+        Write-Host "$(hostname) " -ForegroundColor Magenta -NoNewLine
+        # works as well. the hostname variable seems redundant
+    }
+    if ($admin)  {
+        $sym = '#'
+    }
+    else {
+        $sym = '$'
+    }
+    Write-Host "$($wd) " -ForegroundColor Cyan -NoNewLine
+    if ($gitStatus) {
+        Write-Host "`b" -NoNewline
+        Write-Host (Write-VcsStatus) -NoNewline
+    }
+    #>
+    Write-Host ''
+    Write-Host "$($sym)" -ForegroundColor Cyan -NoNewLine
+    return ' '
 }
 
 Invoke-Expression "function Reset-Prompt { . '$($MyInvocation.MyCommand.Path)' }"
